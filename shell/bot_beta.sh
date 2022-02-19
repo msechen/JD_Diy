417890f3277fb8d608fc028675eafd60021d66a2
msechen <74814336+msechen@users.noreply.github.com>
2022-02-19 23:08:50 +0800

    Update bot_beta.sh

--- shell/bot_beta.sh
+++ shell/bot_beta.sh
@@ -11,7 +11,7 @@ dir_diybot=$root/repo/diybot
 file_bot=$root/config/bot.json
 file_botset=$root/config/botset.json
 file_diybotset=$root/config/diybotset.json
-url="https://github.com/chiupam/JD_Diy.git"
+url="https://github.com/msechen/JD_Diy.git"
 
 git_clone() {
     local url=$1
