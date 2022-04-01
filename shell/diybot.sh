8c4dd9d1b963fd2b23628f96e12f2fafceadad52
msechen <74814336+msechen@users.noreply.github.com>
2022-04-01 22:38:46 +0800

    Update diybot.sh

--- shell/diybot.sh
+++ shell/diybot.sh
@@ -6,6 +6,7 @@ else
   root=/ql
 fi
 
+rm -rf $root/own/raw/*
 git clone https://github.com/msechen/jdrain.git $root/own/raw
 cp -f $root/own/raw/jdCookie.js $root/scripts
 cp -f $root/own/raw/sendNotify.js $root/scripts
