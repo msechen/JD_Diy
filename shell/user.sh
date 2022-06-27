06d1ae30951ae11343ebef3ff5bf1b3a9dc8dffe
msechen <74814336+msechen@users.noreply.github.com>
2022-06-27 16:00:45 +0800

    Update user.sh

--- shell/user.sh
+++ shell/user.sh
@@ -12,7 +12,8 @@ pip3 install telethon --upgrade
 dir_jbot=$root/jbot
 dir_diy=$dir_jbot/diy
 dir_repo=$root/repo
-url="https://raw.githubusercontent.com/msechen/JD_Diy/main/jbot/diy/user.py"
+#url="https://raw.githubusercontent.com/msechen/JD_Diy/main/jbot/diy/user.py"
+url="https://raw.githubusercontent.com/msechen/JD_Diy/main/jbot/user/login.py"
 file_user=$dir_diy/user.py
 bot_set=$root/config/diybotset.json
 
