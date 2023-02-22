f381b3d174531c22a9484166526dfdfdc94fba0f
msechen <74814336+msechen@users.noreply.github.com>
2023-02-22 17:48:10 +0800

    #用法 1、先去私聊狗哥号https://t.me/wogouge私发 注册 获取token,填写到上方  2、回复一个带京东链接、淘宝链接、淘宝口令的消息 比价，拉取历史比价数据  3、有任何使用问题反馈给狗哥https://t.me/wogouge

--- jbot/diy/bj.py
+++ jbot/diy/bj.py
@@ -6,10 +6,17 @@ from urllib import parse
 import asyncio
 from .user import client
 
-#私聊狗哥获取token
+#私聊狗哥https://t.me/wogouge获取token
 user_token=''
 
-@client.on(events.NewMessage(pattern=r'^bj', outgoing=True))
+#用法
+1、先去私聊狗哥号https://t.me/wogouge私发 注册 获取token,填写到上方
+
+2、回复一个带京东链接、淘宝链接、淘宝口令的消息 比价，拉取历史比价数据
+
+3、有任何使用问题反馈给狗哥https://t.me/wogouge
+
+@client.on(events.NewMessage(pattern=r'^比价', outgoing=True))
 async def bj_bot(event):
     msg_text = await event.get_reply_message() 
     messages=str(msg_text).split("message='")[1].split("',",1)[0]
@@ -19,7 +26,6 @@ async def bj_bot(event):
         for message in messages:
             if "u.jd.com" in message or "item.jd.com" in message or "item.m.jd.com" in message or "kpl.m.jd.com" in message or '₴' in message or '₵' in message or '£' in message or '€' in message or '₤' in message or '�' in message or '(' in message or ")" in message or "￥" in message or "$" in message or "₳" in message or "¢" in message or "m.tb.cn" in message or "?" in message:
                 SuperConvertUrl=message
-                await event.edit(SuperConvertUrl)
         header = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
         }
